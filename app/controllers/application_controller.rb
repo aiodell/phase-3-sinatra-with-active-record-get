f583f10906d30c81d5a408d05b553e5707a67889
Andrianna <andrianna.odell@gmail.com>
2022-08-22 08:40:38 -0400

    commit tree abbbae1567cf90fde45acbbeaa37828b97972b95

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,29 @@ class ApplicationController < Sinatra::Base
     { message: "Hello world" }.to_json
   end
 
+  # Add this line to set the Content-Type header for all responses
+  set :default_content_type, 'application/json'
+
+  get '/games' do
+    # get all the games from the server
+    games = Game.all.order( :title ).limit(10)
+    # return a JSON response with an array of all the game data
+    games.to_json
+  end
+
+    # Add this line to set the Content-Type header for all responses
+  set :default_content_type, 'application/json'
+
+   get '/games/:id' do
+    # look up the game in the database using its ID
+    game = Game.find(params[:id])
+    # send a JSON-formatted response of the game data
+    # include the reviews from the data
+     game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
+  end
+
 end
